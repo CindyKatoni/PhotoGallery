f6f5b9127ec23a63e1664c7b002f902e49a47ab5
CindyKatoni <katmuema@gmail.com>
2020-10-10 23:32:53 +0300

    view function for home page

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from photos.models import Photos
 
 # Create your views here.
+def photo_index(request):
+    photos = Photos.objects.all()
+    context = {
+        'projects':projects
+    }
+    return render(request, 'photo_index.html', context)
